9798739ed35b8534170ca97b6eabedab3f25aba9
Dennis Michael Piquero <109423880+DENCIO28@users.noreply.github.com>
2024-05-09 14:52:49 +0800

    Add files via upload

---
+++ lab2-set-b/widgets/content.php
@@ -0,0 +1,14 @@
+<h2 class="content-text">Hot Items</h2>
+    
+<div class="listing">
+            
+    <?php
+        for ($i =0; $i<10; $i++){
+    ?>
+    <?php
+        include ($product_card);
+    ?>
+    <?php
+        }
+    ?>
+</div>
